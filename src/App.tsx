8bdfa09c6b37f94a5f5422e3c0cbbce5625bc2b4
Farah Nasreen <farahnasreen2000@gmail.com>
2022-11-29 19:37:32 +0530

    Impl update and delete functions

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export type todoType = {
 const App: FC = () => {
   const [text, setText] = useState<string>("");
   const [todos, setTodos] = useState<todoType[]>([]);
-
+  const [isUpdating, setNewUpdating] = useState("");
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
   };
@@ -21,35 +21,73 @@ const App: FC = () => {
   };
   useEffect(() => {
     listOfTodos();
-  });
+  }, []);
 
   const addTodo = async () => {
-    let response = await fetch("http://localhost:8080/todo/create", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text }),
-    });
-    if (response.ok) {
-      setText("");
-      listOfTodos();
+    if (isUpdating === "") {
+      let response = await fetch("http://localhost:8080/todo/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text }),
+      });
+      if (response.ok) {
+        setText("");
+        listOfTodos();
+      }
+    } else {
+      const data = { id: isUpdating, text };
+      let response = await fetch("http://localhost:8080/todo/update", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ data }),
+      });
+      if (response.ok) {
+        setText("");
+        listOfTodos();
+        setNewUpdating("");
+        console.log("inside update2");
+      }
     }
+  };
+
+  const updateTodo = (id: string, todoName: string) => {
+    setNewUpdating(id);
+    setText(todoName);
+    console.log("inside update1");
+  };
+
+  const deleteTodo = async (id: string) => {
+    let response = await fetch("http://localhost:8080/todo/delete", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    });
+    console.log("inside delete fn", response);
+    listOfTodos();
   };
 
   return (
     <div className="App">
-      <div>
+      <div className="container">
         <h1>Todo App</h1>
-        <div>
+        <div className="top">
           <input
             type="text"
             value={text}
-            placeholder="Enter Todos"
+            placeholder="Enter Todos..."
             onChange={handleChange}
           />
-          <button onClick={addTodo}>Add</button>
+          <button className="add" onClick={addTodo}>
+            {isUpdating ? "update" : "Add"}
+          </button>
         </div>
         {todos.map((todo: todoType) => (
-          <Item key={todo._id} todoName={todo.todoName} />
+          <Item
+            key={todo._id}
+            todoName={todo.todoName}
+            updateTodo={() => updateTodo(todo._id, todo.todoName)}
+            deleteTodo={() => deleteTodo(todo._id)}
+          />
         ))}
       </div>
     </div>
