1882e08c40454f350dc97e24a23493884c79ca1b
Farah Nasreen <farahnasreen2000@gmail.com>
2022-11-29 16:40:30 +0530

    Add API to send data to the server

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ const App: FC = () => {
   const [text, setText] = useState<string>("");
   const [todos, setTodos] = useState<todoType[]>([]);
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
   const listOfTodos = async () => {
     let response = await fetch("http://localhost:8080/todo/get");
     let rawTodo = await response.json();
@@ -19,10 +22,32 @@ const App: FC = () => {
   useEffect(() => {
     listOfTodos();
   });
+
+  const addTodo = async () => {
+    let response = await fetch("http://localhost:8080/todo/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+    if (response.ok) {
+      setText("");
+      listOfTodos();
+    }
+  };
+
   return (
     <div className="App">
       <div>
         <h1>Todo App</h1>
+        <div>
+          <input
+            type="text"
+            value={text}
+            placeholder="Enter Todos"
+            onChange={handleChange}
+          />
+          <button onClick={addTodo}>Add</button>
+        </div>
         {todos.map((todo: todoType) => (
           <Item key={todo._id} todoName={todo.todoName} />
         ))}
